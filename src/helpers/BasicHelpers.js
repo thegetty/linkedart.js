7fc09265078b269e5d3df73ed5d7c64ab8a9b796
Charles Webb <chwebb@getty.edu>
2021-07-06 20:11:34 -0400

    update normalizeFieldToArray doc

--- src/helpers/BasicHelpers.js
+++ src/helpers/BasicHelpers.js
@@ -6,12 +6,20 @@
  */
 
 /**
- * Checks whether the object has the key as a property, and returns the result as either an empty array (if it doesn't exist), and array with the single object if it's an object or primative, or the array
+ * Checks whether the object has the key as a property, and returns the result as either 
+ * an empty array (if it doesn't exist), an array with the single object if it's an object 
+ * or primitive, or the array
  *
- * @param {object} obj
- * @param {string} key
+ * @param {object} obj - The object that might have an array attribute
+ * @param {string} key - The key for the attribute field we are interested in
+ * 
+ * @example field doesn't exist
+ * normalizeFieldToArray({"id": "1234"}, "type") would return an empty array ([])
+ * 
+ * @example field value is not an array
+ * normalizeFieldToArray({"id": "1234"}, "id") would return ["1234"]
  *
- * @returns {array} - undefined is treated as an arry
+ * @returns {array} - The field's value normalized to an array
  */
 export function normalizeFieldToArray(obj, key) {
   if (obj == undefined) {
