b56d518ee9ee6eb29cf4d03b90c2ddc887e26538
Nabil Kashyap <n@nabilk.com>
2021-10-29 13:13:08 -0700

    adds simple dimensions description helper

---
+++ src/helpers/ObjectHelpers.spec.js
@@ -0,0 +1,11 @@
+import * as objectHelpers from "./ObjectHelpers";
+
+import irises from "../data/mocks/c88b3df0-de91-4f5b-a9ef-7b2b9a6d8abb.json";
+
+describe("getDimensionsDescription", () => {
+  it("gets the correct dimensions description if present", () => {
+    expect(objectHelpers.getDimensionsDescription(irises)).toEqual(
+      "74.3 × 94.3 cm (29 1/4 × 37 1/8 in.)"
+    );
+  });
+});
